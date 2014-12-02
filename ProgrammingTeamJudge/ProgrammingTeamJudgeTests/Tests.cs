02984567de77a0633e59749ab1166b27801d03ce
Grey Newell <ahteeim@gmail.com>
2014-12-01 21:11:59 -0500

    Added main method to new 'Tests' class. Solution builds.

---
+++ ProgrammingTeamJudge/ProgrammingTeamJudgeTests/Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgrammingTeamJudgeTests
+{
+    class Tests
+    {
+        public static void Main() { }
+    }
+}
